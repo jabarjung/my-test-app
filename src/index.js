bea19edf5563b55a2f05d43ba77d3c567c865a2e
jabarjung <jabarjung@yahoo.ca>
2019-03-11 21:44:44 -0700

    Created a simple React component and rendered it on the DOM i.e. showed it in the browser.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+  return <div>Hello there!</div>;
+}
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+  <App/>, document.querySelector('#root')
+  );
