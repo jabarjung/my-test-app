7ba50b4ed97ba68fd7449558821180c85721a17d
jabarjung <jabarjung@yahoo.ca>
2019-03-13 14:56:11 -0700

    Converted the HTML styles to JSX styles. Use double quotes for JSX strings and single quotes everywhere else.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom';
 
 // Create a React component
 const App = () => {
-  return <div>Hello there!</div>;
-}
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
 // Take the React component and show it on the screen
-ReactDOM.render(
-  <App/>, document.querySelector('#root')
-  );
+ReactDOM.render(<App />, document.querySelector('#root'));
