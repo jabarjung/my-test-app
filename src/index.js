7f5f48bb1328f00237ec2f0d01f807513641cce2
jabarjung <jabarjung@yahoo.ca>
2019-03-14 18:19:16 -0700

    JS objects can be referenced in JSX as long as we are not printing them on the screen. As you see in this commit, the JS object can be referenced in JSX. Also, we can reference a property of JS object in JSX to print it on the screen. It's just that the JS object can't be printed as whole on the screen.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import ReactDOM from 'react-dom';
 // Create a React component
 const App = () => {
   const buttonText = { text: 'Click me' };
+  const style = { backgroundColor: 'blue', color: 'white' };
   return (
     <div>
       <label className="label" for="name">
         Enter name:
       </label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText}
-      </button>
+      <button style={style}>{buttonText.text}</button>
     </div>
   );
 };
