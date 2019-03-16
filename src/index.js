714a858497682598d43a4b6fb6289686b2ed39b8
jabarjung <jabarjung@yahoo.ca>
2019-03-15 19:08:44 -0700

    A super simple component to start with Section 3.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ import ReactDOM from 'react-dom';
 
 // Create a React component
 const App = () => {
-  const buttonText = { text: 'Click me' };
-  const labelText = 'Enter name:';
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
-    </div>
-  );
+  return <div>Hello there!</div>;
 };
 
 // Take the React component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
-// Section 1 and 2 are now complete.
