53fce0869a5131c495f41b3b446c141843a48786
jabarjung <jabarjung@yahoo.ca>
2019-03-18 17:48:35 -0700

    All of the components are fully configurable by the parent. Avatar is also being passed down from parent to child and it's rendered in the child. Stephen Grider might have a different way of doing that so let's see. It could be the same or different but we will see and that's the reason I am committing this code for record.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 // Create a React component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45 PM"
+        content="Nice work!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00 AM"
+        content="It's good."
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00 PM"
+        content="It's brilliant."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
