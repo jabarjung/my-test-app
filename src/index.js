57d263c3c310d99cf6e38e5fbf9ee763a69f2453
jabarjung <jabarjung@yahoo.ca>
2019-03-18 23:23:01 -0700

    Not only we can pass component (CommentDetail) as a child to another component (ApprovalCard) while rendering/creating that component (ApprovalCard) but we can also pass plain text or any other JSX object. We can pass any number of components as children.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           author="Sam"
           timeAgo="Today at 4:45 PM"
@@ -17,18 +23,22 @@ const App = () => {
           avatar={faker.image.avatar()}
         />
       </ApprovalCard>
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00 AM"
-        content="It's a good piece of writing."
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00 PM"
-        content="It's a brilliant idea."
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00 AM"
+          content="It's a good piece of writing."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          content="It's a brilliant idea."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
