5380739b5e932b3d24fee170e5b6efed8c92c94f
jabarjung <jabarjung@yahoo.ca>
2019-03-18 19:57:36 -0700

    Created a new component called 'ApprovalCard'. Now going to put an option to insert component 'CommentDetail' into 'ApprovalCard' so that we can show component nesting as well as the component reusability. Also, we will have the ability to show any custom content in addition to 'CommentDetail' so that we practice component reusability property properly and fully.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 // Create a React component
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45 PM"
