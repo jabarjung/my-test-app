e3dc2cef474ca5204ac956fb1f070b365a4b5c83
jabarjung <jabarjung@yahoo.ca>
2019-03-19 17:27:02 -0700

    Just setting up the section 4.

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SeasonDisplay = props => {
+  return (
+    <div>
+      Random text
+      <div>Random text</div>
+      <div>Random text</div>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,11 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay';
 
 // Create a React component
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <div>
-          <h4>Warning!</h4>
-          Are you sure you want to do this?
-        </div>
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4:45 PM"
-          content="Nice work!"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Alex"
-          timeAgo="Today at 2:00 AM"
-          content="It's a good piece of writing."
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Jane"
-          timeAgo="Yesterday at 5:00 PM"
-          content="It's a brilliant idea."
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
-  );
+  return <div>Hello there!</div>;
 };
 
 // Take the React component and show it on the screen
