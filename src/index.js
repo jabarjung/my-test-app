61259c2b7584ca2bba0ebe643aca2012b7888699
jabarjung <jabarjung@yahoo.ca>
2019-03-21 15:50:39 -0700

    We have just converted a function-based component into a class-based component but we are still not using the 'state' functionality of React.js yet.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-// Create a React component
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err),
-  );
-  return <div>Hello there!</div>;
-};
+// Create a class-based React component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err),
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 // Take the React component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
