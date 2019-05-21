c44d6e92e43669d3400e215267ac48d31fb50caa
jabarjung <jabarjung@yahoo.ca>
2019-05-21 16:21:58 -0700

    Made use of ternary expression in JSX.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
-    return latitude > 0 ? 'Summer' : 'Winter';
+    return latitude > 0 ? 'summer' : 'winter';
   } else {
-    return latitude > 0 ? 'Winter' : 'Summer';
+    return latitude > 0 ? 'winter' : 'summer';
   }
 };
 
 const SeasonDisplay = props => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+  const text =
+    season === 'winter' ? 'Burr, it is chilly.' : 'Let us hit the beach.';
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
