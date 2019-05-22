1e8f87d0bd39b4f0af0d32cf4d04a5328c908910
jabarjung <jabarjung@yahoo.ca>
2019-05-21 18:08:10 -0700

    Created an object for configurations for Season and extracted those properties/values. Replaced the redundant ternary expressions as well. It improved the readability and legability of the code.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr, it is chilly!',
+    iconName: 'snowflake',
+  },
+};
+
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
     return latitude > 0 ? 'summer' : 'winter';
@@ -10,14 +21,12 @@ const getSeason = (latitude, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  const text =
-    season === 'winter' ? 'Burr, it is chilly.' : 'Let us hit the beach.';
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
