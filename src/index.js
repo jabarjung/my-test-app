a5b54746dd89fa6e7bca0db1fe96c3e11cc97756
jabarjung <jabarjung@yahoo.ca>
2019-05-23 20:13:32 -0700

    Avoid conditionals in render() function because if you have conditionals then you would have multiple return statements in render() function and just in case you want to apply a common element or property to all of them then it becomes way harder. The way to do it is to move the conditionals to a separate function and then take care of the common element or property when it comes back from that function to render() function. I have also added a comment regarding this in the 'index.js' file as well.

--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ class App extends React.Component {
     );
   }
 
-  // For every component (or class component) we create, we have to define a render() method
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.latitude) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -25,6 +24,19 @@ class App extends React.Component {
       return <SeasonDisplay latitude={this.state.latitude} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  // For every component (or class component) we create, we have to define a render() method
+  render() {
+    return <div>{this.renderContent()}</div>;
+    /* The <div> just above could be assigned a className or property and that would be
+    the common element or property of whatever is returned from renderContent(). The logic is
+    to avoid any conditionals in render() function and then we won't have multiple return
+    statements and just in case we have to apply a common element or property to all of them
+    then we have to assign that element manually to each of them unless we take all that
+    logic/conditionals and move them to a separate function and whatever is returned from that
+    function is already assigned a common element or property by render() function.
+    */
   }
 }
 
