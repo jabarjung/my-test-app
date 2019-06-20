0ec1fc991c6a660f8b541aac9871c6e95772c81c
jabarjung <jabarjung@yahoo.ca>
2019-06-20 15:21:44 -0700

    Invoking callback in child to the parent.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,7 @@ class SearchBar extends React.Component {
   state = { term: '' };
   onFormSubmit = event => {
     event.preventDefault();
+    this.props.onSubmit(this.state.term);
   };
   render() {
     return (
