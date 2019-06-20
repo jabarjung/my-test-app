89379b53f56d2d6b6abd9dd02626d070c853d583
jabarjung <jabarjung@yahoo.ca>
2019-06-19 19:05:04 -0700

    This would be the default way to fix 'this' callback issues. Just write the arrow function in the function definition.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: '' };
-  onFormSubmit(event) {
+  onFormSubmit = event => {
     event.preventDefault();
-  }
+  };
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={e => this.onFormSubmit(e)} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
