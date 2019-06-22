49a48ba5e308845b0b4027f80d4d426e68cee5f1
jabarjung <jabarjung@yahoo.ca>
2019-06-22 14:49:10 -0700

    Either we can work with 'promises' and chain .then statements. That's one way of doing it. Alternatively we can use 'async await' syntax. It's much preferred and better way of handling network requests and the data which is returned back.

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization:
-            'Client-ID 63c8de006eae26a8006455d86fb6049b82dd61e0338ea4c27a7f9cd515eaba53',
-        },
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 63c8de006eae26a8006455d86fb6049b82dd61e0338ea4c27a7f9cd515eaba53',
+      },
+    });
+    console.log(response.data.results);
   }
   render() {
     return (
