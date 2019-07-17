1ed6ae1e97545f2657a2921a7bdef81f716a6052
jabarjung <jabarjung@yahoo.ca>
2019-07-16 19:47:21 -0700

    Section 11 is complete.

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
-    this.onTermSubmit('babies');
+    this.onTermSubmit('tom and jerry');
   }
 
   onTermSubmit = async term => {
